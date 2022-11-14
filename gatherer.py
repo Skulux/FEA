f19950ec7d2dee3b5c14b37a57cee651b455d4d2
Skulux <Emilis0627@gmail.com>
2022-11-14 16:18:25 +0100

    +Trending Algo +get trending +get genres +id search

--- gatherer.py
+++ gatherer.py
@@ -5,8 +5,7 @@ KEY = "e93c3350ce19973913e4baf37a49a213"
 lang = "de-DE"
 
 
-def search_by_name(term, title=False, poster=False, date=False, rating=False, overview=False, *args, **kwargs):
-    settings = [title, poster, date, rating, overview]
+def search_by_name(term, title=False, poster=False, date=False, rating=False, overview=False, **kwargs):
     try:
         if kwargs["enable_all"]:
             title = True
@@ -46,28 +45,116 @@ def search_by_name(term, title=False, poster=False, date=False, rating=False, ov
                 pass
     return titles
 
-def load():
-    time.sleep(1)
-    for x in range(10):
-        print("\n" * 100)
-        time.sleep(0.15)
-        print(".")
-        time.sleep(0.15)
-        print("\n" * 100)
-        time.sleep(0.15)
-        print("..")
-        time.sleep(0.15)
-        print("\n" * 100)
-        time.sleep(0.15)
-        print("...")
-        time.sleep(0.15)
-        print("\n" * 100)
+
+def search_by_id(movie_id, title=False, poster=False, date=False, rating=False, overview=False, **kwargs):
+    try:
+        if kwargs["enable_all"]:
+            title = True
+            poster = True
+            date = True
+            rating = True
+            overview = True
+    except:
+        pass
+    query = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={KEY}"
+    res = requests.get(query).json()
+    return [res["original_title"] if title else None,
+            "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + str(res["poster_path"]) if poster else None,
+            res["release_date"] if date else None,
+            res["vote_average"] if rating else None,
+            res["overview"] if overview else None]
 
 
-print("Welcome To FEA v0.1")
-print("Loading...")
-time.sleep(2)
-term = input("Title: ")
-sus = search_by_name(term, enable_all=True)
-print(len(sus))
-print(sus)
+def get_genres(movie_id: int):
+    query = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={KEY}"
+    response = requests.get(query)
+    ids = []
+    try:
+        for x in response.json()["genres"]:
+            try:
+                ids += [x["id"]]
+            except:
+                pass
+    except:
+        pass
+    return ids
+
+
+def get_genre_list(movie_list: list, return_dict=False):
+    genre_ranking = {}
+    ids = []
+    for movie_id in movie_list:
+        ids += [get_genres(movie_id)]
+    for id_sets in ids:
+        for id_ in id_sets:
+            if id_ not in genre_ranking:
+                genre_ranking[id_] = 1
+            else:
+                genre_ranking[id_] += 1
+    genre_ranking = {k: v for k, v in sorted(genre_ranking.items(), key=lambda item: item[1], reverse=True)}
+    if return_dict:
+        return genre_ranking
+    return list(genre_ranking)[0], list(genre_ranking)[1], list(genre_ranking)[2]
+
+
+def get_trending(time_span: str="week", title=False, poster=False, date=False, rating=False, overview=False, **kwargs):
+    try:
+        if kwargs["enable_all"]:
+            title = True
+            poster = True
+            date = True
+            rating = True
+            overview = True
+    except:
+        pass
+    titles = {}
+    for page in range(1, 10):
+        query = f"https://api.themoviedb.org/3/trending/all/{time_span}?api_key={KEY}&page={page}"
+        response = requests.get(query)
+        for x in response.json()["results"]:
+            try:
+                titles[x["id"]] = [x["original_title"] if title else None,
+                                   "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + str(
+                                       x["poster_path"]) if poster else None,
+                                   x["release_date"] if date else None,
+                                   x["vote_average"] if rating else None,
+                                   x["overview"] if overview else None]
+            except:
+                pass
+    return titles
+
+
+def compare_genres(movie_list: list):
+    fav_genres = get_genre_list(movie_list)
+    trending_genres = {}
+    for id_ in list(get_trending("week").keys()):
+        trending_genres[id_] = get_genres(id_)
+    best_score = 0
+    movies = None
+    for movie in trending_genres:
+        temp_score = 0
+        for genre in trending_genres[movie]:
+            for genre_ in fav_genres:
+                if genre_ == genre:
+                    temp_score += 1
+        if temp_score > best_score:
+            best_score = temp_score
+            movies = movie
+            print(best_score)
+    return movies
+
+
+
+
+
+#print("Welcome To FEA v0.1")
+#print("Loading...")
+#term = input("Title: ")
+#sus = search_by_name(term, enable_all=True)
+#print(len(sus))
+#print(sus)
+#print(get_genres(808))
+#print(search_by_id(808, enable_all=True))
+#print(get_genre_list([810, 505, 808, 809, 505], return_dict=True))
+#print(get_trending( enable_all=True))
+print(compare_genres([664469, 808, 809, 810, 505]))
