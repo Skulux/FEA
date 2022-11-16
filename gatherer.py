2dcc80b9b397e3d61c1dfbdade98217edf534108
Skulux <Emilis0627@gmail.com>
2022-11-16 19:23:11 +0100

    +sessions

--- gatherer.py
+++ gatherer.py
@@ -1,6 +1,6 @@
-import time
-import random
+from random import choice
 import requests
+from collections import Counter
 KEY = "e93c3350ce19973913e4baf37a49a213"
 lang = "de-DE"
 
@@ -41,7 +41,7 @@ def search_by_name(term, title=False, poster=False, date=False, rating=False, ov
         f"&page={page}" \
         f"&include_adult=True" \
         f"&query={term}"
-        response = requests.get(query)
+        response = s.get(query)
         for x in response.json()["results"]:
             try:
                 titles[x["id"]] = [x["original_title"] if title else None,
@@ -73,7 +73,7 @@ def search_by_id(movie_id, title=False, poster=False, date=False, rating=False, 
         rating = True
         overview = True
     query = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={KEY}"
-    res = requests.get(query).json()
+    res = s.get(query).json()
     return [res["original_title"] if title else None,
             "https://image.tmdb.org/t/p/w600_and_h900_bestv2" + str(res["poster_path"]) if poster else None,
             res["release_date"] if date else None,
@@ -88,7 +88,7 @@ def get_genres(movie_id: int):
     :return: list of genre ids
     """
     query = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={KEY}"
-    response = requests.get(query)
+    response = s.get(query)
     ids = []
     try:
         for x in response.json()["genres"]:
@@ -108,17 +108,10 @@ def get_genre_list(movie_list: list, return_dict=False):
     :param return_dict: bool return all genres ranked as dict
     :return: list of top 3 genres or dict of all genres ranked
     """
-    genre_ranking = {}
     ids = []
     for movie_id in movie_list:
-        ids += [get_genres(movie_id)]
-    for id_sets in ids:
-        for id_ in id_sets:
-            if id_ not in genre_ranking:
-                genre_ranking[id_] = 1
-            else:
-                genre_ranking[id_] += 1
-    genre_ranking = {k: v for k, v in sorted(genre_ranking.items(), key=lambda item: item[1], reverse=True)}
+        ids += [genre for genre in get_genres(movie_id)]
+    genre_ranking = Counter(ids)
     if return_dict:
         return genre_ranking
     return list(genre_ranking)[0], list(genre_ranking)[1], list(genre_ranking)[2]
@@ -144,9 +137,9 @@ def get_trending(time_span: str="week", title=False, poster=False, date=False, r
         rating = True
         overview = True
     titles = {}
-    for page in range(1, 100):
+    for page in range(1, 6):
         query = f"https://api.themoviedb.org/3/trending/all/{time_span}?api_key={KEY}&page={page}"
-        response = requests.get(query)
+        response = s.get(url=query)
         for x in response.json()["results"]:
             try:
                 titles[x["id"]] = [x["original_title"] if title else None,
@@ -168,36 +161,13 @@ def compare_genres(movie_list: list, offset: int = 0):
     :return: int movie id
     """
     fav_genres = get_genre_list(movie_list)
-    trending_genres = {}
-    for id_ in list(get_trending("week").keys()):
-        trending_genres[id_] = get_genres(id_)
     movies = []
-    for movie in trending_genres:
-        temp_score = 0
-        for genre in trending_genres[movie]:
-            for genre_ in fav_genres:
-                if genre_ == genre:
-                    temp_score += 1
-        if temp_score == len(fav_genres) - offset and movie not in movie_list:
-            movies += [movie]
-    if not movies and offset < 3:
+    for id_ in list(get_trending("week").keys()):
+        if len(set(get_genres(id_)) & set(fav_genres)) >= len(fav_genres) - offset:
+            movies += [id_]
+    if not movies and offset < 2:
         compare_genres(movie_list, offset + 1)
-    return random.choice(movies)
-    # TODO: Add Multiprocessing?
+    return choice(movies)
 
 
-
-if __name__ == "__main__":
-    #print("Welcome To FEA v0.1")
-    #print("Loading...")
-    #term = input("Title: ")
-    #sus = search_by_name(term, enable_all=True)
-    #print(len(sus))
-    #print(sus)
-    #print(get_genres(808))
-    #print(search_by_id(808, enable_all=True))
-    #print(get_genre_list([810, 505, 808, 809, 505], return_dict=True))
-    start = time.time()
-    print(get_trending(enable_all=True))
-    print(time.time() - start)
-    #print(compare_genres([664469, 808, 809, 810, 505]))
+s = requests.session()
