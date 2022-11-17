749ea63062aa6af2ac62a4030a0e744162b18ca4
Skulux <Emilis0627@gmail.com>
2022-11-17 19:01:08 +0100

    +sorting by status

--- Fae_Datenbank.py
+++ Fae_Datenbank.py
@@ -1,7 +1,7 @@
 import os
 import sqlobject
 
-db_filename = os.path.abspath('Faedatenbank.db')
+db_filename = os.path.abspath('FEA.db')
 connection_string = 'sqlite:' + db_filename
 connection = sqlobject.connectionForURI(connection_string)
 sqlobject.sqlhub.processConnection = connection
@@ -10,8 +10,7 @@ sqlobject.sqlhub.processConnection = connection
 class Watchlist(sqlobject.SQLObject):
     movie_id = sqlobject.IntCol()
     rating = sqlobject.FloatCol()
-    status = sqlobject.StringCol()
-    progress = sqlobject.IntCol(default=0)
+    status = sqlobject.IntCol()
     comment = sqlobject.StringCol(length=250)
 
 
@@ -24,26 +23,28 @@ def createTables():
     return None
 
 
-def get_all_movies_data(sort_rating=False, sort_status=False, reverse=False):
+def get_all_movies_data(sort_rating=False, sort_status=False):
     """
     Gets all Watchlist Entries attributes
     :return: dict with id as key and list of attributes as values
     """
     mv = list(Watchlist.select())
-    p = [{x.movie_id: [x.rating, x.status, x.progress, x.comment]} for x in mv]
+    p = [{x.movie_id: [x.rating, x.status, x.comment]} for x in mv]
     new = {}
     newnew = {}
     for x in p:
         new.update(x)
-    for y in new:
+    if sort_rating or sort_status:
+        n = 0 if sort_rating else 1
+        for y in new:
+            for x in new:
+                if new[x][n] > new[y][n]:
+                    print(new[x][n], new[y][n])
+                    newnew[x] = new[x]
         for x in new:
-            if not reverse and new[x][0] < new[y][0]:
+            if x not in newnew:
                 newnew[x] = new[x]
-            elif new[x][0] > new[y][0]:
-                newnew[x] = new[x]
-    for x in new:
-        if x not in newnew:
-            newnew[x] = new[x]
+        return newnew
     return new
 
 
@@ -64,31 +65,27 @@ def get_movie_data_by_id(movie_id: int):
     :return: list of Entry attributes
     """
     movie = list(Watchlist.select(Watchlist.q.movie_id == movie_id))
-    return [[mv.movie_id, mv.rating, mv.status, mv.progress, mv.comment] for mv in movie][0]
+    return [[mv.movie_id, mv.rating, mv.status, mv.comment] for mv in movie][0]
 
 
-def insert_data(movie_id: int, rating: float, status: str, progress: int, comment: str):
+def insert_data(movie_id: int, rating: float, status: int, comment: str):
     """
     Create new Watchlist Entry
     :param movie_id: int
     :param rating: float between 0.0 - 10.0
-    :param status: string (Plan, Watching, Done)
-    :param progress: int
+    :param status: int (1: Planned, 2: Watching, 3: Done)
     :param comment: str
     :return: Watchlist Object
     """
-    mv = Watchlist(movie_id=movie_id, rating=rating, status=status, progress=progress,
-                   comment=comment)
-    return mv
+    return Watchlist(movie_id=movie_id, rating=rating, status=status, comment=comment)
 
 
-def update_data(movie_id: int, rating: float = None, status: str = None, progress: int = None, comment: str = None):
+def update_data(movie_id: int, rating: float = None, status: int = None, comment: str = None):
     """
     Updates Watchlist Entries
     :param movie_id: int
     :param rating: float between 0.0 - 10.0
-    :param status: string (Plan, Watching, Done)
-    :param progress: int
+    :param status: int (1: Planned, 2: Watching, 3: Done)
     :param comment: str
     :return: None
     """
@@ -99,8 +96,6 @@ def update_data(movie_id: int, rating: float = None, status: str = None, progres
                 mv.rating = rating
             if status:
                 mv.status = status
-            if progress:
-                mv.progress = progress
             if comment:
                 mv.comment = comment
         except Exception as ER:
@@ -122,5 +117,8 @@ def delete_entry(id_):
 
 
 createTables()
+#insert_data(808, 9, 3, "")
+#insert_data(809, 8, 2, "")
+#insert_data(810, 0, 1, "")
 #update_data(475557, rating=9.8)
-print(get_all_movies_data(sort_rating=True, reverse=False))
+#print(get_all_movies_data(sort_status=True))
