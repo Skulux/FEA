02a1a08a584bbbfecb65b23617bc9e2d25c50fd3
Skulux <Emilis0627@gmail.com>
2022-12-12 13:21:53 +0100

    +config changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 import time
 
 
-def create_config():
+def create():
     config = configparser.ConfigParser()
     if os.path.isfile('config.ini'):
         os.remove('config.ini')
@@ -21,14 +21,38 @@ def setup():
         theme = config.get('GENERAL', 'theme')
         lang = config.get('GENERAL', 'language')
         if theme not in ['dark', 'light']:
-            create_config()
+            create()
             return "light", "de"
         return theme, lang
     except:
-        create_config()
+        create()
         return "light", "de"
 
 
+def read():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    theme = config.get('GENERAL', 'theme')
+    lang = config.get('GENERAL', 'language')
+    return theme, lang
 
 
-print(setup())
+def change(theme=None, language=None):
+    config = configparser.ConfigParser()
+    try:
+        config.read('config.ini')
+        if theme:
+            theme = config.set('GENERAL', 'theme', theme)
+        if language:
+            lang = config.set('GENERAL', 'language', language)
+        if theme or language:
+            with open(r"config.ini", "w") as configfile:
+                config.write(configfile)
+        theme = config.get('GENERAL', 'theme')
+        lang = config.get('GENERAL', 'language')
+        return theme, lang
+    except Exception as ERR:
+        print(ERR)
+
+
+print(change(language="de"))
