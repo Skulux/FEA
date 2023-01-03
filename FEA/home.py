148f2f96436f3734ef4e4059f9bfe3ea2a46b979
Skulux <Emilis0627@gmail.com>
2023-01-03 13:21:13 +0100

    + functional Event Buttons

--- FEA/home.py
+++ FEA/home.py
@@ -9,7 +9,11 @@
 
 import wx
 import wx.xrc
-
+import io
+import requests
+import gatherer as API
+import Fae_Datenbank as DB
+from PIL import Image
 ###########################################################################
 ## Class frame_main
 ###########################################################################
@@ -30,7 +34,7 @@ class frame_main ( wx.Frame ):
 
 		boxsizer_header.Add( ( 15, 0), 0, wx.EXPAND, 5 )
 
-		self.bitmap_logo = wx.StaticBitmap( self, wx.ID_ANY, wx.Bitmap( u"assets/logo.png", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		self.bitmap_logo = wx.StaticBitmap( self, wx.ID_ANY, wx.Bitmap( u"assets\\logo.png", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
 		boxsizer_header.Add( self.bitmap_logo, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
 
 		self.staticText_projectname = wx.StaticText( self, wx.ID_ANY, u"FEA", wx.DefaultPosition, wx.DefaultSize, 0 )
@@ -82,7 +86,7 @@ class frame_main ( wx.Frame ):
 
 		self.button_home = wx.BitmapButton( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, wx.BU_AUTODRAW|wx.BORDER_NONE )
 
-		self.button_home.SetBitmap( wx.Bitmap( u"assets/home.png", wx.BITMAP_TYPE_ANY ) )
+		self.button_home.SetBitmap( wx.Bitmap( u"assets\\home.png", wx.BITMAP_TYPE_ANY ) )
 		self.button_home.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 		self.button_home.SetBackgroundColour( wx.Colour( 24, 24, 24 ) )
 
@@ -93,7 +97,7 @@ class frame_main ( wx.Frame ):
 
 		self.button_watchlist = wx.BitmapButton( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, wx.BU_AUTODRAW|wx.BORDER_NONE )
 
-		self.button_watchlist.SetBitmap( wx.Bitmap( u"assets/list.png", wx.BITMAP_TYPE_ANY ) )
+		self.button_watchlist.SetBitmap( wx.Bitmap( u"assets\\list.png", wx.BITMAP_TYPE_ANY ) )
 		self.button_watchlist.SetBackgroundColour( wx.Colour( 24, 24, 24 ) )
 
 		boxsizer_sideElements.Add( self.button_watchlist, 0, wx.ALL, 5 )
@@ -103,7 +107,7 @@ class frame_main ( wx.Frame ):
 
 		self.button_like = wx.BitmapButton( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, wx.BU_AUTODRAW|wx.BORDER_NONE )
 
-		self.button_like.SetBitmap( wx.Bitmap( u"assets/like.png", wx.BITMAP_TYPE_ANY ) )
+		self.button_like.SetBitmap( wx.Bitmap( u"assets\\like.png", wx.BITMAP_TYPE_ANY ) )
 		self.button_like.SetBackgroundColour( wx.Colour( 24, 24, 24 ) )
 
 		boxsizer_sideElements.Add( self.button_like, 0, wx.ALL, 5 )
@@ -113,7 +117,7 @@ class frame_main ( wx.Frame ):
 
 		self.button_settings = wx.BitmapButton( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, wx.BU_AUTODRAW|wx.BORDER_NONE )
 
-		self.button_settings.SetBitmap( wx.Bitmap( u"assets/settings.png", wx.BITMAP_TYPE_ANY ) )
+		self.button_settings.SetBitmap( wx.Bitmap( u"assets\\settings.png", wx.BITMAP_TYPE_ANY ) )
 		self.button_settings.SetBackgroundColour( wx.Colour( 24, 24, 24 ) )
 
 		boxsizer_sideElements.Add( self.button_settings, 0, wx.ALL, 5 )
@@ -128,6 +132,8 @@ class frame_main ( wx.Frame ):
 
 		boxsizer_topRow = wx.BoxSizer( wx.VERTICAL )
 
+		self.m_scrolledWindow1 = wx.ScrolledWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.VSCROLL )
+		self.m_scrolledWindow1.SetScrollRate( 5, 5 )
 		boxsizer_moviesTop = wx.BoxSizer( wx.HORIZONTAL )
 
 
@@ -135,10 +141,10 @@ class frame_main ( wx.Frame ):
 
 		boxsizer_movie1 = wx.BoxSizer( wx.VERTICAL )
 
-		self.bitmap_movie1 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie1.Add( self.bitmap_movie1, 0, wx.ALL, 5 )
+		self.m_bpButton5 = wx.BitmapButton( self.m_scrolledWindow1, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), wx.BU_AUTODRAW|0 )
+		boxsizer_movie1.Add( self.m_bpButton5, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
 
-		self.staticText_movie1 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.staticText_movie1 = wx.StaticText( self.m_scrolledWindow1, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.staticText_movie1.Wrap( -1 )
 
 		self.staticText_movie1.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
@@ -152,157 +158,14 @@ class frame_main ( wx.Frame ):
 
 		boxsizer_moviesTop.Add( ( 0, 0), 1, wx.EXPAND, 5 )
 
-		boxsizer_movie2 = wx.BoxSizer( wx.VERTICAL )
 
-		self.bitmap_movie2 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie2.Add( self.bitmap_movie2, 0, wx.ALL, 5 )
-
-		self.staticText_movie11 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie11.Wrap( -1 )
-
-		self.staticText_movie11.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie11.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie2.Add( self.staticText_movie11, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesTop.Add( boxsizer_movie2, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesTop.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie3 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie3 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie3.Add( self.bitmap_movie3, 0, wx.ALL, 5 )
-
-		self.staticText_movie2 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie2.Wrap( -1 )
-
-		self.staticText_movie2.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie2.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie3.Add( self.staticText_movie2, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesTop.Add( boxsizer_movie3, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesTop.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie1 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie4 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie1.Add( self.bitmap_movie4, 0, wx.ALL, 5 )
-
-		self.staticText_movie3 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie3.Wrap( -1 )
-
-		self.staticText_movie3.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie3.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie1.Add( self.staticText_movie3, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesTop.Add( boxsizer_movie1, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesTop.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-
-		boxsizer_topRow.Add( boxsizer_moviesTop, 0, wx.EXPAND, 5 )
+		self.m_scrolledWindow1.SetSizer( boxsizer_moviesTop )
+		self.m_scrolledWindow1.Layout()
+		boxsizer_moviesTop.Fit( self.m_scrolledWindow1 )
+		boxsizer_topRow.Add( self.m_scrolledWindow1, 1, wx.EXPAND |wx.ALL, 5 )
 
 
 		boxsizer_movies.Add( boxsizer_topRow, 1, wx.EXPAND, 5 )
-
-		boxsizer_bottomRow = wx.BoxSizer( wx.VERTICAL )
-
-		boxsizer_moviesBottom = wx.BoxSizer( wx.HORIZONTAL )
-
-
-		boxsizer_moviesBottom.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie5 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie5 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie5.Add( self.bitmap_movie5, 0, wx.ALL, 5 )
-
-		self.staticText_movie5 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie5.Wrap( -1 )
-
-		self.staticText_movie5.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie5.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie5.Add( self.staticText_movie5, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesBottom.Add( boxsizer_movie5, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesBottom.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie6 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie6 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie6.Add( self.bitmap_movie6, 0, wx.ALL, 5 )
-
-		self.staticText_movie6 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie6.Wrap( -1 )
-
-		self.staticText_movie6.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie6.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie6.Add( self.staticText_movie6, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesBottom.Add( boxsizer_movie6, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesBottom.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie7 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie7 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie7.Add( self.bitmap_movie7, 0, wx.ALL, 5 )
-
-		self.staticText_movie7 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie7.Wrap( -1 )
-
-		self.staticText_movie7.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie7.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie7.Add( self.staticText_movie7, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesBottom.Add( boxsizer_movie7, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesBottom.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-		boxsizer_movie8 = wx.BoxSizer( wx.VERTICAL )
-
-		self.bitmap_movie8 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.Size( 210,265 ), 0 )
-		boxsizer_movie8.Add( self.bitmap_movie8, 0, wx.ALL, 5 )
-
-		self.staticText_movie8 = wx.StaticText( self, wx.ID_ANY, u"MyLabel", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.staticText_movie8.Wrap( -1 )
-
-		self.staticText_movie8.SetFont( wx.Font( 14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous" ) )
-		self.staticText_movie8.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
-
-		boxsizer_movie8.Add( self.staticText_movie8, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
-
-
-		boxsizer_moviesBottom.Add( boxsizer_movie8, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_moviesBottom.Add( ( 0, 0), 1, wx.EXPAND, 5 )
-
-
-		boxsizer_bottomRow.Add( boxsizer_moviesBottom, 1, wx.EXPAND, 5 )
-
-
-		boxsizer_movies.Add( boxsizer_bottomRow, 1, wx.EXPAND, 5 )
 
 
 		boxsizer_body.Add( boxsizer_movies, 1, wx.EXPAND, 5 )
@@ -316,7 +179,126 @@ class frame_main ( wx.Frame ):
 
 		self.Centre( wx.BOTH )
 
+		# Connect Events
+		self.searchCtrl_moviesearch.Bind( wx.EVT_SEARCHCTRL_SEARCH_BTN, self.on_search )
+		self.button_suprise.Bind( wx.EVT_TOGGLEBUTTON, self.on_suprise )
+		self.button_home.Bind( wx.EVT_BUTTON, self.on_home )
+		self.button_watchlist.Bind( wx.EVT_BUTTON, self.on_watchlist )
+		self.button_like.Bind( wx.EVT_BUTTON, self.on_like )
+		self.button_settings.Bind( wx.EVT_BUTTON, self.on_settings )
+		self.m_bpButton5.Bind( wx.EVT_BUTTON, self.on_movie )
+
 	def __del__( self ):
 		pass
 
+
+	# Virtual event handlers, override them in your derived class
+	def on_search( self, event ):
+		event.Skip()
+
+	def on_suprise( self, event ):
+		self.m_scrolledWindow1.DestroyChildren()
+		trends = [API.search_by_id(API.compare_genres(DB.get_all_movie_ids()), enable_all=True)]
+		sizers_movie = []
+
+		for movie in trends:
+			self.m_bpButton5 = wx.BitmapButton(self.m_scrolledWindow1, wx.ID_ANY, cnvrt_bmp(movie[1]), wx.DefaultPosition,
+											   wx.Size(210, 315), wx.BU_AUTODRAW | 0)
+			sizers_movie += [wx.BoxSizer(wx.VERTICAL)]
+			sizers_movie[-1].Add(self.m_bpButton5, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+			self.staticText_movie1 = wx.StaticText(self.m_scrolledWindow1, wx.ID_ANY, u""+movie[0], wx.DefaultPosition,
+												   wx.DefaultSize, 0)
+			self.staticText_movie1.Wrap(-1)
+			self.staticText_movie1.SetFont(
+				wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous"))
+			self.staticText_movie1.SetForegroundColour(wx.Colour(255, 255, 255))
+			sizers_movie[-1].Add(self.staticText_movie1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+		sizer_sizer = []
+		boxsizer_moviesTop = wx.BoxSizer(wx.HORIZONTAL)
+		for sizer in sizers_movie:
+			boxsizer_moviesTop.Add(sizer, 1, wx.EXPAND, 5)
+		boxsizer_moviesTop.Add((0, 0), 1, wx.EXPAND, 5)
+		self.m_scrolledWindow1.SetSizer(boxsizer_moviesTop)
+		self.m_scrolledWindow1.Layout()
+
+	def on_home( self, event ):
+		self.m_scrolledWindow1.DestroyChildren()
+		trends = API.get_trending(enable_all=True, limit=2)
+		sizers_movie = []
+
+		for movie in trends:
+			self.m_bpButton5 = wx.BitmapButton(self.m_scrolledWindow1, wx.ID_ANY, cnvrt_bmp(trends[movie][1]), wx.DefaultPosition,
+											   wx.Size(210, 315), wx.BU_AUTODRAW | 0)
+			sizers_movie += [wx.BoxSizer(wx.VERTICAL)]
+			sizers_movie[-1].Add(self.m_bpButton5, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+			self.staticText_movie1 = wx.StaticText(self.m_scrolledWindow1, wx.ID_ANY, u""+trends[movie][0], wx.DefaultPosition,
+												   wx.DefaultSize, 0)
+			self.staticText_movie1.Wrap(-1)
+			self.staticText_movie1.SetFont(
+				wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous"))
+			self.staticText_movie1.SetForegroundColour(wx.Colour(255, 255, 255))
+			sizers_movie[-1].Add(self.staticText_movie1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+		sizer_sizer = []
+		boxsizer_moviesTop = wx.BoxSizer(wx.HORIZONTAL)
+		for sizer in sizers_movie:
+			boxsizer_moviesTop.Add(sizer, 1, wx.EXPAND, 5)
+		boxsizer_moviesTop.Add((0, 0), 1, wx.EXPAND, 5)
+		self.m_scrolledWindow1.SetSizer(boxsizer_moviesTop)
+		self.m_scrolledWindow1.Layout()
+
+	def on_watchlist( self, event ):
+		self.m_scrolledWindow1.DestroyChildren()
+		movies = []
+
+		for movie in DB.get_all_movie_ids():
+			movies += [API.search_by_id(movie, enable_all=True)]
+
+		sizers_movie = []
+
+		for movie in movies:
+			try:
+				self.m_bpButton5 = wx.BitmapButton(self.m_scrolledWindow1, wx.ID_ANY, cnvrt_bmp(movie[1]), wx.DefaultPosition,
+												   wx.Size(210, 315), wx.BU_AUTODRAW | 0)
+				sizers_movie += [wx.BoxSizer(wx.VERTICAL)]
+				sizers_movie[-1].Add(self.m_bpButton5, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+				self.staticText_movie1 = wx.StaticText(self.m_scrolledWindow1, wx.ID_ANY, u""+movie[0], wx.DefaultPosition,
+													   wx.DefaultSize, 0)
+				self.staticText_movie1.Wrap(-1)
+				self.staticText_movie1.SetFont(
+					wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Righteous"))
+				self.staticText_movie1.SetForegroundColour(wx.Colour(255, 255, 255))
+				sizers_movie[-1].Add(self.staticText_movie1, 0, wx.ALIGN_CENTER | wx.ALL, 5)
+			except:
+				pass
+		sizer_sizer = []
+		boxsizer_moviesTop = wx.BoxSizer(wx.HORIZONTAL)
+		for sizer in sizers_movie:
+			boxsizer_moviesTop.Add(sizer, 1, wx.EXPAND, 5)
+		boxsizer_moviesTop.Add((0, 0), 1, wx.EXPAND, 5)
+		self.m_scrolledWindow1.SetSizer(boxsizer_moviesTop)
+		self.m_scrolledWindow1.Layout()
+
+	def on_like( self, event ):
+		event.Skip()
+
+	def on_settings( self, event ):
+		event.Skip()
+
+	def on_movie( self, event ):
+		event.Skip()
+
+
+
+def cnvrt_bmp(link):
+	try:
+		bmp = requests.get(link)
+		img = Image.open(io.BytesIO(bmp.content))
+		img = img.save("temp.bmp")
+		bmp = wx.Bitmap("temp.bmp")
+	except:
+		bmp = wx.Bitmap("assets\\missing.bmp")
+	finally:
+		bmp.Rescale(bmp, (210, 265))
+		bmp = wx.Bitmap(bmp)
+		return bmp
 
